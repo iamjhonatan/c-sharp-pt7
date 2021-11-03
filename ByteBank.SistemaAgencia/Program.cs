4651f0dd01ac4c4b9cb55bf3b5c3982d81f87ca8
Jhonatan Medeiros <jhonatanmedeiros7@gmail.com>
2021-11-03 15:28:05 -0300

    Conhecendo array

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -12,6 +12,18 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
+            // Array de inteiros com 5 posições
+            
+            int [] idades = new int[5];
+
+            idades[0] = 15;
+            idades[1] = 28;
+            idades[2] = 35;
+            idades[3] = 50;
+            idades[4] = 55;
+        
+            Console.WriteLine(idades[4]);
+            
             Console.ReadLine();
         }
     }
