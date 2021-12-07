0138570a782b48a4e0ee026891cd838983a1bee8
Jhonatan Medeiros <jhonatanmedeiros7@gmail.com>
2021-12-06 21:51:34 -0300

    Inicializador de array

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -12,6 +12,24 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
+            ContaCorrente[] contas = new ContaCorrente[]
+            {
+                new ContaCorrente(873, 1234),
+                new ContaCorrente(873, 1235),
+                new ContaCorrente(873, 1236)
+            };
+
+            for (int indice = 0; indice < contas.Length; indice++)
+            {
+                ContaCorrente contaAtual = contas[indice];
+                Console.WriteLine($"Conta {indice} {contaAtual.Numero}");
+            }
+                
+            Console.ReadLine();
+        }
+        
+        static void TestaArrayInt()
+        {
             // Array de inteiros com 5 posições
             
             int [] idades = new int[6];
@@ -22,7 +40,6 @@ namespace ByteBank.SistemaAgencia
             // idades[3] = 50;
             // idades[4] = 55;
             // idades[5] = 60;
-            
 
             int acumulador = 0;
             for (int indice = 0; indice < idades.Length; indice++)
@@ -38,9 +55,6 @@ namespace ByteBank.SistemaAgencia
             int media = acumulador / idades.Length;
             Console.WriteLine($"Média de idades = {media}");
 
-            Console.ReadLine();
-            
-            
             // ** Ctrl + k + c = comenta várias linhas
         }
     }
